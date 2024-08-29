ea95e0cf656aae52c8d5cf7544abfdaefd1cf29d
Ambuj Shukla <absurdusername@icloud.com>
2024-08-29 21:09:52 +0530

    Create utils.py

---
+++ CV/utils.py
@@ -0,0 +1,41 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from math import ceil
+
+def plot_images(*args, ncols=-1):
+    """
+    Plot images side-by-side using matplotlib.
+
+    Parameters:
+        *args: list of image and titles
+        ncols (int): maximum number of images in each row of the plot
+
+    Example usage:
+    >>> plot_images(image1, "title 1", image2, image3, image4, "title 4")
+    """
+    pairs = []
+
+    for a in args:
+        if type(a) == str:
+            pairs[-1][-1] = a
+        else:
+            pairs.append([a, ''])
+
+    fig, axes = plt.subplots(ncols=len(pairs))
+    
+    for i, (image, title) in enumerate(pairs):
+        ax = axes[i] if len(pairs) > 1 else axes
+        ax.imshow(image, cmap='grey')
+        ax.set_title(title)
+        ax.axis('off')
+    
+    plt.show()
+
+
+def display_images(title, *images):
+    vis = np.concat(images, axis=1)
+    cv2.imshow(title, vis)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    cv2.waitKey(1)
