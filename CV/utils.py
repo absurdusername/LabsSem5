c0b51d95d9cb8a798fa45ab3cc28eaa16760f9e6
Ambuj Shukla <absurdusername@icloud.com>
2024-08-30 17:45:24 +0530

    Refactor

--- CV/utils.py
+++ CV/utils.py
@@ -2,32 +2,40 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_images(*args, ncols=-1):
+
+def _parse_image_title_pairs(L):
+    """Extract (image, title) pairs from flattened list L."""
+    pairs = []
+
+    for item in L:
+        if type(item) == str:
+            pairs[-1][-1] = item
+        else:
+            pairs.append([item, ''])
+    
+    return pairs
+
+
+def plot_images(*args):
     """
     Plot images side-by-side using matplotlib.
 
     Parameters:
-        *args: list of image and titles
-        ncols (int): maximum number of images in each row of the plot
+        *args: images and titles
 
     Example usage:
     >>> plot_images(image1, "title 1", image2, image3, image4, "title 4")
     """
-    pairs = []
-
-    for a in args:
-        if type(a) == str:
-            pairs[-1][-1] = a
-        else:
-            pairs.append([a, ''])
+    pairs = _parse_image_title_pairs(args)
 
     fig, axes = plt.subplots(ncols=len(pairs))
+    for ax in axes.flat:
+        ax.axis('off')
     
     for i, (image, title) in enumerate(pairs):
         ax = axes[i] if len(pairs) > 1 else axes
         ax.imshow(image, cmap='grey')
         ax.set_title(title)
-        ax.axis('off')
     
     plt.show()
 
@@ -37,3 +45,4 @@ def display_image(title, image):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
     cv2.waitKey(1)
+    
